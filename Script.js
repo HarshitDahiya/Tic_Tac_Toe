fe6452d98f21dc3c5abe65ce7e705d9790caf04d
Harshit Dahiya <Harshitdahiya816@gmail.com>
2018-08-07 21:05:30 +0530

    Third Commit ()

--- Script.js
+++ Script.js
@@ -26,7 +26,12 @@ function startGame() {
 }
 
 function turnClick(square) {
+	if (typeof origBoard[square.target.id] == 'number') {
 	turn(square.target.id, humanP);
+	if (!checkTie()) {
+		turn(bestSpot(), aiP);
+	}
+	}
 }
 
 function turn(squareId, player) {
@@ -57,5 +62,31 @@ function gameOver(gameWon) {
 	}
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == humanP ? "You Win!" : "You Lose!");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return emptySquares()[0];
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', turnClick, false);
+			declareWinner("Tie Game!");
+			return true;
+		}
+		return false;
 	}
 }
